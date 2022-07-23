72ac3f91a98c94c23aff3424335f02570f122b20
Muhammet Yunus TUNCA <muhammedytunca@gmail.com>
2022-07-24 01:49:26 +0300

    contact btn

---
+++ docs/functions.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector('#contactBtn').onclick = () => window.open('https://wa.me/905516046200?text=Merhaba%2C%0AEMDATE%20Acil%20G%C3%BCc%C3%BC%20gruplar%C4%B1ndan%20birine%20kat%C4%B1lmak%20istiyorum.%0AYard%C4%B1mc%C4%B1%20olabilir%20misiniz%3F')
+})
